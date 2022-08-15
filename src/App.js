9c7205facea62e98c59208ac94cb9ef34b881e14
Yurii <paraska108@gmail.com>
2022-08-15 17:00:36 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="main">
+          <h1 className="main_header">Everything You Love About Coffee</h1>
+          <div className="logo">
+            <div className="logo-divider divider"></div>
+            <div className="logo-img"><img src="" alt="" /></div>
+          </div>
+          <h2 className="main_subheader">We makes every day full of energy and taste
+            Want to try our beans?</h2>
+          <button>More</button>
+        </div>
       </header>
+      <nav>
+        <img src="./img/bg/Group.png" alt="logo" />
+        <ul>
+          <li>Coffee house</li>
+          <li>Our coffee</li>
+          <li>For your pleasure</li>
+        </ul>
+      </nav>
+
     </div>
   );
 }
