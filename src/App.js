d32bc51b328a1f8a194c9799711f4ecfeeb95f3c
Yurii <paraska108@gmail.com>
2022-08-16 15:13:16 +0300

    header-main pages

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ function App() {
           <h1 className="main_header">Everything You Love About Coffee</h1>
           <div className="logo">
             <div className="logo-divider divider"></div>
-            <div className="logo-img"><img src="" alt="" /></div>
+            <div className="logo-img"><img src="./img/bg/Group.png" alt="" /></div>
           </div>
           <h2 className="main_subheader">We makes every day full of energy and taste
             Want to try our beans?</h2>
           <button>More</button>
         </div>
       </header>
-      <nav>
-        <img src="./img/bg/Group.png" alt="logo" />
-        <ul>
-          <li>Coffee house</li>
-          <li>Our coffee</li>
-          <li>For your pleasure</li>
-        </ul>
-      </nav>
+      {/* <div className="nav_main"> */}
+        <nav>
+          <div className="logo_img"></div>
+          <ul>
+            <li><a href="https://www.facebook.com/profile.php?id=100013564327758">Coffee house
+            </a></li>
+            <li><a href="https://www.facebook.com/profile.php?id=100013564327758">Our coffee
+            </a></li>
+            <li><a href="https://www.facebook.com/profile.php?id=100013564327758">For your pleasure
+            </a></li>
+          </ul>
+        </nav>
+      {/* </div> */}
 
     </div>
   );
