afcb144962195bb71d845fd966086cf43a671b59
Yurii <paraska108@gmail.com>
2022-08-16 16:14:59 +0300

    header-main pages

--- src/App.js
+++ src/App.js
@@ -6,28 +6,31 @@ function App() {
       <header>
         <div className="main">
           <h1 className="main_header">Everything You Love About Coffee</h1>
-          <div className="logo">
-            <div className="logo-divider divider"></div>
-            <div className="logo-img"><img src="./img/bg/Group.png" alt="" /></div>
+          <div className="main_logo">
+            <div className="main_logo_divider divider"></div>
+            <div className="main_logo_img"><img src={require('./img/bg/coffee-beans-white-sec.svg').default} alt='logo'/></div>
+            <div className="main_logo_divider divider"></div>
           </div>
-          <h2 className="main_subheader">We makes every day full of energy and taste
-            Want to try our beans?</h2>
+          <h2 className="main_subheader">We makes every day full of energy and taste</h2>
+            <h2 className="main_subheader">Want to try our beans?</h2>
           <button>More</button>
         </div>
       </header>
-      {/* <div className="nav_main"> */}
-        <nav>
-          <div className="logo_img"></div>
-          <ul>
-            <li><a href="https://www.facebook.com/profile.php?id=100013564327758">Coffee house
+      <nav>
+
+        <ul>
+          <li>
+            <div className="logo_img">
+              <img src={require('./img/bg/coffee-beans-white.svg').default} alt='logo'/>
+            </div>
+            <a href="https://www.facebook.com/profile.php?id=100013564327758">Coffee house
             </a></li>
-            <li><a href="https://www.facebook.com/profile.php?id=100013564327758">Our coffee
-            </a></li>
-            <li><a href="https://www.facebook.com/profile.php?id=100013564327758">For your pleasure
-            </a></li>
-          </ul>
-        </nav>
-      {/* </div> */}
+          <li><a href="https://www.facebook.com/profile.php?id=100013564327758">Our coffee
+          </a></li>
+          <li><a href="https://www.facebook.com/profile.php?id=100013564327758">For your pleasure
+          </a></li>
+        </ul>
+      </nav>
 
     </div>
   );
